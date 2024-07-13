ea25ed75306afe9b5f44cf1482e96efc4d52ed10
amjay.shin <amjay.31@gmail.com>
2024-07-13 15:58:29 +0900

    initialize project

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+
+        /*
+        // 1. jpql을 동적으로 생성
+        em.createQuery("select o from Order o join o.member m" +
+                                "where o.status = :status" +
+                                "and m.name like :name",
+                        Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+        */
+
+        //==2.
+        String jpql = "";
+        em.createQuery(jpql, Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+
+    }
+
+}
